c83341a1718e4714eba0fd2b2af18ccec539a9d9
Yuki Matsukura <matsubokkuri@gmail.com>
2013-12-04 00:24:28 +0900

    Moved

--- index.php
+++ index.php
@@ -8,31 +8,25 @@ $current = (int)@$_REQUEST['num'] ?: 15;
     <meta name="author" content="Yuki Matsukura">
   </head>
   <body>
-<h1>キャッシュテスト</h1>
+<h1>FP表示テスト</h1>
 
 <ul>
 <li>This page is encoded with UTF-8. Please report if you have any difficulties reading following sentence.</li>
-<li>1枚50.9KBです。10枚表示されれば500KB以上のコンテンツを表示出来るであると判断する。</li>
-<li>画像はブラウザにキャッシュされないようにURLをユニークに生成しています。</li>
+<li><a href="cache.php">ブラウザキャッシュの容量を検証</a></li>
+<li><a href="emoji.php">auのUTF8対応を検証</a></li>
 </ul>
 
-<h1>表示枚数変更</h1>
-<form action="?" method="get">
-<select name="num">
-<?php foreach (range(1, 100) as $i) { ?>
-<option value="<?= $i ?>" <?php if ($current == $i) print 'selected="selected"'; ?>><?= $i ?></option>
-<?php } ?>
-</select>
 
-<input type="submit" value="表示">
+<h1>環境変数</h1>
 
-</form>
+<dl>
+<?php
+foreach ($_SERVER as $key => $value) {
+    print sprintf("<dt>%s</dt><dd>%s</dd>", $key, $value);
+}
+?>
+</dl>
 
-<h1>サンプル画像</h1>
-<?php foreach (range(1, $current) as $i) { ?>
-<div><?= $i ?>枚目↓</div>
-<img src="out.png?<?php print mt_rand(0, 1000000); ?>">
-<?php } ?>
 
   </body>
 </html>
